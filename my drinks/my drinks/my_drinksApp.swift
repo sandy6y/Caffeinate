8a372cfb07f3367f43e4d7432fb962abcb430201
syy6 <syy6@cornell.edu>
2026-04-28 16:26:38 -0400

    addfile

---
+++ my drinks/my drinks/my_drinksApp.swift
@@ -0,0 +1,17 @@
+//
+//  my_drinksApp.swift
+//  my drinks
+//
+//  Created by Sandy Yang on 4/28/26.
+//
+
+import SwiftUI
+
+@main
+struct my_drinksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
